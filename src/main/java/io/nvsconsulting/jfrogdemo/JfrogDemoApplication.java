e2c730ca744af5757d4bda0a79d5dd699260505a
Philip Foster <103243270+pfoster-rht@users.noreply.github.com>
2023-07-18 14:40:43 -0500

    Add project files

---
+++ src/main/java/io/nvsconsulting/jfrogdemo/JfrogDemoApplication.java
@@ -0,0 +1,13 @@
+package io.nvsconsulting.jfrogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfrogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfrogDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nvsconsulting/jfrogdemo/JfrogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nvsconsulting.jfrogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfrogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
